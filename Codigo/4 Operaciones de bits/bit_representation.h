7d5cc448b8def4355d6fa3109780fb054f4545ea
José Fuentes - Toki <fs.pepe@gmail.com>
2021-10-12 13:08:29 -0300

    Actualización archivos Clase8.pdf y bit_representation.h

--- Codigo/4 Operaciones de bits/bit_representation.h
+++ Codigo/4 Operaciones de bits/bit_representation.h
@@ -1,24 +1,27 @@
+#define BOLD "\e[1m"
+#define WHITE "\e[7m"
+#define BLUE "\e[44m"
+#define NORMAL "\e[0m"
+
 void int2bits(int x) {
   // Número de bits de un entero
   int num_bits = sizeof(int)*8;
   char rep[num_bits];
   int mask = 1;
 
-  printf("\e[1mRepresentación binaria de \e[7m%d\e[0m\n", x);
+  printf("%sRepresentación binaria de %s%d%s\n", BOLD, WHITE, x, NORMAL);
 
   // Cómputo de la representación binaria de x por medio de bit-shifting 
   for(int i=0; i < num_bits; i++, x >>=1)
     rep[i] = (char)(x & mask);
 
   // Impresión de índices
-  printf("\e[37m");
   for(int i=num_bits-1; i >= 0; i--)
     printf("%2d ", i);
-  printf("\n\e[0m");
+  printf("\n");
 
   // Impresión de los bits
-  printf("\e[1m\e[44m");
   for(int i=num_bits-1; i >= 0; i--)
-    printf("%2d ", rep[i]);
-  printf("\e[0m\n");
+    printf("%s%s%2d %s", BOLD, BLUE, rep[i], NORMAL);
+  printf("\n\n");
 }
